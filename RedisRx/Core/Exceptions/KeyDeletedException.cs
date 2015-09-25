f2fef9c06e84eb719d12fda792a76a50d83ce892
censorjam <jones.marc@gmail.com>
2015-09-25 08:31:59 +0100

    f

---
+++ RedisRx/Core/Exceptions/KeyDeletedException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RedisRx.Exceptions
+{
+    public class KeyDeletedException : Exception
+    {
+        public string Key { get; private set; }
+        public KeyDeletedException(string key)
+        {
+            Key = key;
+        }
+    }
+}
