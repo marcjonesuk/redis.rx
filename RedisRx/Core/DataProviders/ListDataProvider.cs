b31d99346968f95dfeda6a22fe39d6fb145074d5
censorjam <jones.marc@gmail.com>
2015-09-25 15:26:23 +0100

    a

---
+++ RedisRx/Core/DataProviders/ListDataProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace RedisRx.DataProviders
+{
+    public class ListDataProviderAsync : IDataProviderAsync<RedisValue[]>
+    {
+        private readonly IDatabase _database;
+
+        public ListDataProviderAsync(IDatabase database)
+        {
+            _database = database;
+        }
+
+        public Task<RedisValue[]> GetNext(string key)
+        {
+            return _database.ListRangeAsync(key);
+        }
+    }
+}
